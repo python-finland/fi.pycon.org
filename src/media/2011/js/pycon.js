b3d3ec88be1bbd45f8345dd61713b693dd394977
Jyrki Pulliainen <jyrki@taiste.fi>
2011-08-03 20:52:47 +0300

    2011: Add a scroller navigation

---
+++ src/media/2011/js/pycon.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('body > header a').click(function(e) {
+        var target = $(this.hash);
+        $('body > header a.focus').removeClass('focus');
+        if (target.length) {
+            var offset = target.offset().top - 100;
+            $('html,body').animate({scrollTop: offset}, 1000);
+            $(this).addClass('focus');
+        }
+        e.stopPropagation();
+    });
+});
