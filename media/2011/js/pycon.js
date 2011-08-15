7504df6f0aea82da7e4f1a8e1b3edeb2bc50b8fc
Jyrki Pulliainen <jyrki@taiste.fi>
2011-08-15 19:25:09 +0300

    Style page

--- media/2011/js/pycon.js
+++ media/2011/js/pycon.js
@@ -139,7 +139,9 @@
 
         $('body > header a').click(function(e) {
             var target = $(this.hash);
-            _gaq.push(['_trackEvent', 'Navigation', this.hash.slice(1)]);
+            if (typeof(_gaq) !== "undefined") {
+                _gaq.push(['_trackEvent', 'Navigation', this.hash.slice(1)]);
+            }
             $('body > header a.focus').removeClass('focus');
             if (target.length) {
                 var offset = target.offset().top;
