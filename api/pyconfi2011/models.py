c1137b076a60b310996eb9d2588955aced744bc3
Jyrki Pulliainen <jyrki@taiste.fi>
2011-08-15 22:18:38 +0300

    Add additional model fields

--- api/pyconfi2011/models.py
+++ api/pyconfi2011/models.py
@@ -4,8 +4,18 @@ from django.db import models
 class Registration(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    ticket_type = models.CharField(max_length='30', choices=(
+    ticket_type = models.CharField(max_length=30, choices=(
         ('normal', 'Normal'),
         ('corporate', 'Corporate'),
         ('student', 'Student'),
     ))
+
+    dinner = models.BooleanField(default=True)
+
+    billing_address = models.CharField(max_length=100)
+    billing_zipcode = models.CharField(max_length=15)
+    billing_city = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+
+    billed = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False)
