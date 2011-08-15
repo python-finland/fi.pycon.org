a4f07adb1439c9a9e83e8a0cde0a9a8f984d9bd2
Jyrki Pulliainen <jyrki@taiste.fi>
2011-08-15 22:27:49 +0300

    Add snailmail_bill field

--- api/pyconfi2011/models.py
+++ api/pyconfi2011/models.py
@@ -9,13 +9,14 @@ class Registration(models.Model):
         ('corporate', 'Corporate'),
         ('student', 'Student'),
     ))
+    country = models.CharField(max_length=2)
 
     dinner = models.BooleanField(default=True)
 
+    snailmail_bill = models.BooleanField(default=False)
     billing_address = models.CharField(max_length=100)
     billing_zipcode = models.CharField(max_length=15)
     billing_city = models.CharField(max_length=100)
-    country = models.CharField(max_length=2)
 
     billed = models.BooleanField(default=False)
     paid = models.BooleanField(default=False)
