77b78e5185664c2333004e1d345337d1a1e69976
Hieu Nguyen <ndhieu88@gmail.com>
2012-09-24 13:59:42 +0300

    Change index.html and pycon2012.js to not use Django template, separate front-end and back-end logic

--- media/2012/js/pycon2012.js
+++ media/2012/js/pycon2012.js
@@ -28,27 +28,34 @@ $(document).ready(function(){
     });
 
     function mangleHash(hash) {
-        return hash.replace(/[#!]/g, "");
+        return hash.replace(/[#]/g, "");
     }
 
     function navigateTo(divId) {
-        var $target = $("#" + divId);
-        //console.log("Would navigate to ", divId, $target);
+        var target = $("#" + divId);
+        //console.log("Would navigate to ", divId, target);
         
         if (typeof(_gaq) !== "undefined") { // track with Google Analytics
             _gaq.push(['_trackEvent', 'Navigation', divId]);
         }
 
         if (divId == 'sponsors') { // scroll down!
-            $('html,body').animate({scrollTop: $target.offset().top}, 500);
+            $('html,body').animate({scrollTop: target.offset().top}, 500);
         } else { // just fade content in
             $('.page:visible').fadeOut();
             $('ul#menu li a.active').removeClass('active');
             $('ul#menu a[href$=#' + divId + ']').addClass('active');
-            $("#content").animate({height: $target.height() + 60});
-            $target.fadeIn();
+            
+            // local content from html
+            target.load('../2012/_' + divId + '.html', function() {
+                $("#content").animate({height: target.height() + 60});
+                target.fadeIn();
+                if (divId === 'registration') {
+                    initialize_registration();
+                }
+            });
         }
-        location.hash = "#!" + divId;
+        location.hash = "#" + divId;
         
     }
 
@@ -93,96 +100,99 @@ $(document).ready(function(){
         $('#registration form span.price').html(price + ' &euro;');
     };
 
-    $('#id_ticket_type').change(function() {
-        var val = $(this).val(),
-            is_corporate = (val == 'corporate'),
-            is_normal = (val == 'normal');
-        $('#dinner-disclaimer').toggle(is_corporate || is_normal);
-        $('#id_dinner').attr('checked', is_corporate || is_normal);
-        $('#companywrapper').toggle(is_corporate);
-        if(!mobilize.isMobile()) {
-            $("#content").animate({height: $('div#registration').height() + 60});
-        }
-        update_price();
-    });
+    var initialize_registration = function() {
+        $('#id_ticket_type').change(function() {
+            var val = $(this).val(),
+                is_corporate = (val == 'corporate'),
+                is_normal = (val == 'normal');
+            $('#dinner-disclaimer').toggle(is_corporate || is_normal);
+            $('#id_dinner').attr('checked', is_corporate || is_normal);
+            $('#companywrapper').toggle(is_corporate);
+            if(!mobilize.isMobile()) {
+                $("#content").animate({height: $('div#registration').height() + 60});
+            }
+            update_price();
+        });
 
-    $('#id_snailmail_bill').change(function() {
-        $('#billing-details').toggle($(this).is(':checked'));
-        update_price();
-    });
+        $('#id_snailmail_bill').change(function() {
+            $('#billing-details').toggle($(this).is(':checked'));
+            update_price();
+        });
 
-    $('#id_country').typeahead({
-        minLength: 1,
-        items: 10,
-        source: function (query, process) {
-            return $.get('/api/2012/country', { query: query }, function (data) {
-                return process(data);
-            }, 'json');
-        }
-    });
-
-    $('#registration-form').submit(function(e) {
-        e.preventDefault();
-        var self = this;
-
-        $.ajax({
-            url: '/api/2012/register/',
-            type: 'POST',
-            dataType: 'json',
-            data: $(this).serialize(),
-            beforeSend: function(xhr, settings) {
-                $('#thankyou').hide();
-                $(self).find('input, select')
-                    .prop('disabled', true)
-                    .removeClass('error');
-                $('#errorcontainer').hide();
-            },
-            success: function(data, textStatus, xhr) {
-                if(data.ok) {
-                    // Reset the form
-                    self.reset();
-                    $('.input-wrapper input').keyup();
-                    // Show a thankyou message
-                    $('#thankyou').show();
-                    $(self).hide();
-                } else {
-                    var ul = $('#errorcontainer ul');
-                    ul.empty();
-                    $.each(data.errors, function(key, value) {
-                        $('[name=' + key + ']')
-                            .addClass('error');
-                        $.each(value, function(index, error) {
-                            var pretty = key.split('_').join(' ');
-                            ul.append($('<li>').text(pretty + ': ' + error));
-                        });
-                    });
-                    $('#errorcontainer').show();
-                    if(!mobilize.isMobile()) {
-                        $("#content").animate({height: $('div#registration').height() + 60});
-                    }
-                    $(self).find('input, select').prop('disabled', false);
-                }
-            },
-            error: function() {
-                alert("Something went wrong! Try again!");
-            },
-            always: function() {
-                $(self).find('input, select').prop('disabled', false);
+        $('#id_country').typeahead({
+            minLength: 1,
+            items: 10,
+            source: function (query, process) {
+                return $.get('/api/2012/country', { query: query }, function (data) {
+                    return process(data);
+                }, 'json');
             }
         });
-    });
+
+        $('#registration-form').submit(function(e) {
+            e.preventDefault();
+            var self = this;
+
+            $.ajax({
+                url: '/api/2012/register/',
+                type: 'POST',
+                dataType: 'json',
+                data: $(this).serialize(),
+                beforeSend: function(xhr, settings) {
+                    $('#thankyou').hide();
+                    $(self).find('input, select')
+                        .prop('disabled', true)
+                        .removeClass('error');
+                    $('#errorcontainer').hide();
+                },
+                success: function(data, textStatus, xhr) {
+                    if(data.ok) {
+                        // Reset the form
+                        self.reset();
+                        $('.input-wrapper input').keyup();
+                        // Show a thankyou message
+                        $('#thankyou').show();
+                        $(self).hide();
+                    } else {
+                        var ul = $('#errorcontainer ul');
+                        ul.empty();
+                        $.each(data.errors, function(key, value) {
+                            $('[name=' + key + ']')
+                                .addClass('error');
+                            $.each(value, function(index, error) {
+                                var pretty = key.split('_').join(' ');
+                                ul.append($('<li>').text(pretty + ': ' + error));
+                            });
+                        });
+                        $('#errorcontainer').show();
+                        if(!mobilize.isMobile()) {
+                            $("#content").animate({height: $('div#registration').height() + 60});
+                        }
+                        $(self).find('input, select').prop('disabled', false);
+                    }
+                },
+                error: function() {
+                    alert("Something went wrong! Try again!");
+                },
+                always: function() {
+                    $(self).find('input, select').prop('disabled', false);
+                }
+            });
+        });
+    };
 
     // See if we have a fragment and scroll there
     function init() {
         var frag = mangleHash(location.hash);
-        var showIndex = false;
+        //var showIndex = false;
         if(!frag) { frag = 'about'; }
         if(frag == "sponsors"){
             $("#about").fadeIn();
         }
 
         if(frag && !mobilize.isMobile()) {
-            navigateTo(frag, showIndex);
+            navigateTo(frag);
+            // console.log(frag, showIndex)
         }
     }
 
