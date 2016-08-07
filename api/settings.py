62ac75b11977a4d5e7d4078f5f25bcafa9e17060
Tuure Laurinolli <tuure@laurinolli.net>
2016-08-08 00:28:40 +0300

    Don't clobber e-mail sending password

--- api/settings.py
+++ api/settings.py
@@ -91,7 +91,7 @@ EMAIL_HOST_PASSWORD = local_stuff['EMAIL_HOST_PASSWORD']
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = EMAIL_HOST_PASSWORD = local_stuff['STATIC_ROOT']
+STATIC_ROOT = local_stuff['STATIC_ROOT']
 
 
 ROOT_URLCONF = 'api.urls'
